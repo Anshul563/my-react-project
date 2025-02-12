78fa2583f6376f1bc31cb09a9ee72fe1c972aadf
Anshul563 <anshulshakya18168@gmail.com>
2025-02-12 12:21:49 +0530

    Update Hero.jsx

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
+import photo from '../assets/photo.jpg'; // Import the image
 
 const Hero = () => {
     return (
-        <div className='md:flex h-screen md:w-full '>
-            <div className="h-1/2 md:h-screen w-full justify-center items-center  px-6 ">
+        <div className='md:flex h-screen md:w-full'>
+            <div className="h-1/2 md:h-screen w-full justify-center items-center px-6">
                 <h1 className='text-4xl font-extrabold text-[#1f6462] md:pt-38 pb-4 pt-10'>Hello World</h1>
                 <h2 className='text-2xl font-bold text-[#637a82] pb-8'>Web Developer || UI/UX Designer</h2>
                 <p className='text-sm text-[#808e83] pb-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repudiandae ipsum obcaecati in vitae, quidem voluptatibus aliquam similique alias ad non aut eaque, et corporis libero qui perferendis a corrupti.</p>
                 <button className='text-white bg-[#1f6462] border-none py-3 px-7 rounded-sm'>Resume</button>
             </div>
             <div className="h-1/2 md:h-screen w-full">
-                <img className='flex mx-auto h-80 w-80 md:h-screen md:w-full' src="photo.jpg" alt="" />
+                <img className='flex mx-auto h-80 w-80 md:h-screen md:w-full' src={photo} alt="Profile" />
             </div>
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
